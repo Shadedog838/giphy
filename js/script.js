bc8c3e69dbcc43e4f24a37d11cfb8b6fff4b8990
Shadedog838 <malcolm721@gmail.com>
2019-04-02 22:43:22 +0000

    saving work

--- js/script.js
+++ js/script.js
@@ -4,8 +4,19 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+   var choice = $("#search-term").val();
+
+
+    $.ajax({
+        url: "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+ choice,
+        method: "GET",
+        success: function(response) {
+            $('#image').append("<img src=" + response.data[0].images.downsized_large.url + ">");
+console.log(response.data[0].images.downsized_large.url);
+        },
+    });
+
 });
+
+
 
